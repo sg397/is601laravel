f4a9f690cb9bbdf8045a91dca7fef8a31ae6114c
sunitha <sg397@njit.edu>
2018-10-13 18:53:26 -0400

    Feature: Added include files, default layout and home pages

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+ <h1> Hello World Welcome To My About</h1>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+ <h1> Hello World Welcome To My Homepage</h1>
+
+@stop
